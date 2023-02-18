d5cef63202da8b2a8acf2789f0ab88a65c0e2b8d
matan amzaleg <matanamz@gmail.com>
2023-02-18 10:36:17 +0200

    top artists done

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,23 @@
-export const ArtistCard = () => {
-  <div>ArtistCard</div>;
+import { Link, useNavigate } from "react-router-dom";
+
+export const ArtistCard = ({track}) => {
+
+  const navigate = useNavigate()
+
+
+  return (
+    <section className="artist-card-sec flex column" onClick={()=> navigate(`/artists/${track?.artists[0]?.adamid}/${track?.subtitle}`)}>
+      <div className="inside-card relative">
+        <img src={track.images?.background || track.images?.coverart} alt="" />
+      </div>
+      <div className="desc flex column align-center justify-center">
+      <p className="artist-title">
+        {track?.subtitle}
+      </p>
+      </div>
+          </section>
+  )
+
 };
 
 
--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,2 +1,35 @@
-export const TopArtists = () => <div>TopArtists</div>;
+import React, { useEffect } from "react";
+import { loadSongs } from "../redux/actions/songs.actions";
+import { useSelector, useDispatch } from "react-redux";
+import { SongCard } from "../components/SongCard";
+import {ArtistCard} from "../components/ArtistCard";
+
+export const TopArtists = () => {
+
+    const { activeSong, isPlaying, currentSongs } = useSelector((state) => state.songModule);
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      dispatch(loadSongs("TOP_CHARTS"));
+    }, []);
+
+    return (
+        <section className="top-charts-sec flex align-center column">
+            <h1 className="main-title">Top Artists:</h1>
+            <div className="songs-sec">
+            {currentSongs.map((track, i) => {
+          if (track) {
+            return (
+              <ArtistCard
+                key={track.key}
+                track={track}
+              ></ArtistCard>
+            );
+          } else return null;
+        })}
+            </div>
+        </section>
+    )
+}
+
 
