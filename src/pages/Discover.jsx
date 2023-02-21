a1a753018f15aa1f3c427b6f6b30a17b1d9228d8
matan amzaleg <matanamz@gmail.com>
2023-02-21 20:46:28 +0200

    playlists finished

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,26 +5,27 @@ import { genres } from "../assets/constants";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { loadSongs } from "../redux/actions/songs.actions";
-import {RiCloseLine} from 'react-icons/ri';
+import {RiCloseLine, RiH1} from 'react-icons/ri';
 import {HiOutlineMenu} from 'react-icons/hi'
 
-
-
-
 export const Discover = ({setShowModal, setSelectedSong}) => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying, currentSongs } = useSelector((state) => state.songModule);
 
-  const dispacth = useDispatch();
-  const { activeSong, isPlaying, currentSongs} = useSelector((state) => state.songModule);
-  
+  const [loading, setLoading] = useState(true); // Add a state variable to keep track of loading status
+
   useEffect(() => {
-    dispacth(loadSongs());
+    dispatch(loadSongs()).then(() => {
+      setLoading(false); // Set loading to false after the dispatch completes
+    });
   }, []);
 
-  const handleGengreChange = (e)=>{
-    console.log('ok');
-    dispacth(loadSongs(e.target.value));
+  const handleGengreChange = (e) => {
+    setLoading(true); // Set loading to true before the dispatch starts
+    dispatch(loadSongs(e.target.value)).then(() => {
+      setLoading(false); // Set loading to false after the dispatch completes
+    });
   }
-
 
   return (
     <section className="discover-sec flex align-center column">
@@ -40,11 +41,17 @@ export const Discover = ({setShowModal, setSelectedSong}) => {
       </select>
 
       <section className="songs-sec">
-        {currentSongs.map((song, i) => {
-          if(song.images && song.hub.actions){
-            return <SongCard setSelectedSong={setSelectedSong} setShowModal={setShowModal} key={song.key} song={song} i={i} isPlaying={isPlaying} activeSong={activeSong} data={currentSongs}></SongCard>;
-          }else return null
-        })}
+        {loading ? ( 
+          <h1 color="red">Loading...</h1>
+        ) : (
+          currentSongs.map((song, i) => {
+            if(song.images && song.hub.actions){
+              return <SongCard setSelectedSong={setSelectedSong} setShowModal={setShowModal} key={song.key} song={song} i={i} isPlaying={isPlaying} activeSong={activeSong} data={currentSongs}></SongCard>;
+            } else {
+              return null;
+            }
+          })
+        )}
       </section>
     </section>
   );
